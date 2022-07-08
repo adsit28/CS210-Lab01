92bd7679f3e4e642d127006157a52693adedea02
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-08 08:09:56 -0600

    Loading template

---
+++ hello.c
@@ -0,0 +1,25 @@
+/** hello.c
+ * =============================================================
+ * Name: Your Name, Date
+ * Section:  Your Section
+ * Project:  Lab 01
+ * Purpose:  Write a simple program in the C Language
+ * ============================================================= */
+
+// Part 1:  What do you have to import in order to use printf?
+
+
+// Part 2:  What is the name of the function that C always runs when it starts?
+
+
+  // Part 3:  How do you print a phrase?
+  
+  
+  // Part 4:  What does the main function always return?
+  
+
+
+// Part 5:  By the time you get here, you are done.  
+// Open a terminal (in Visual Studio Code, Go to Terminal --> New Terminal)
+//     - Type: gcc -Wall -Werror <filename> -o lab01
+//     - Type: ./lab01 to run your program
