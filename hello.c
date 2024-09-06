a9f9187249efdb11fef556ded0cecc6fa3510672
adsit28 <c26matthew.adsit@afacademy.af.edu>
2024-09-06 11:07:46 -0600

    complete

--- hello.c
+++ hello.c
@@ -1,22 +1,27 @@
 /** hello.c
  * =============================================================
- * Name: Your Name, Date
- * Section:  Your Section
+ * Name: Matthew Adsit, 14 Aug 24
+ * Section:  T3
  * Project:  Lab 01
  * Purpose:  Write a simple program in the C Language
  * ============================================================= */
 
 // Part 1:  What do you have to import in order to use printf?
-
+// <stdio.h>
 
 // Part 2:  What is the name of the function that C always runs when it starts?
-  
+  //main
   
 // Part 3:  What does the main function always return?
-
+//0
 
 // Part 4:  Write the code.
+#include <stdio.h>
 
+int main() {
+    printf("Hello, world! My name is Cadet Adsit\n");
+    return 0;
+}
 
 // Part 5:  Compile, run, then submit your code to zyBooks.
 // Open a terminal (in Visual Studio Code, Go to Terminal --> New Terminal)
